dbb7f2721773796cb960f2e26347b46059c3dcc7
AKSHAY R <140582741+Akshay-eng-hue@users.noreply.github.com>
2024-11-27 19:48:03 +0530

    Update akshay.py

--- akshay.py
+++ akshay.py
@@ -1,6 +1,6 @@
 """
 This module contains an example of iterating over a list of numbers.
 """
-nums = [10, 20, 30, 40]
-for index, value in enumerate(nums):
-    print(f"Index: {index}, Value: {value}")
+nums=[10, 20, 30, 40]
+for index,value in enumerate(nums):
+    print(f"Index:{index},Value:{value}")
