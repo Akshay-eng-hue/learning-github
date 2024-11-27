cd34da2059983ed5885355afb9edd0b4cd289fff
AKSHAY R <140582741+Akshay-eng-hue@users.noreply.github.com>
2024-11-27 18:28:29 +0530

    Update akshay.py

--- akshay.py
+++ akshay.py
@@ -1,9 +1,6 @@
 """
-This module contains the implementation of traversing accross list functionality.
+This module contains an example of iterating over a list of numbers.
 """
-nums=[1,2,4,5,6]
-for i in range(len(nums)):
-    print(nums[i])
-    print(i)
-  
-  
+nums = [10, 20, 30, 40]
+for index, value in enumerate(nums):
+    print(f"Index: {index}, Value: {value}")
