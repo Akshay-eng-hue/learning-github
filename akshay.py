7f4d8ae01f0cf80a56a33dedb4f7dfb5dc91af89
AKSHAY R <140582741+Akshay-eng-hue@users.noreply.github.com>
2024-11-27 18:07:20 +0530

    Update akshay.py

--- akshay.py
+++ akshay.py
@@ -1,10 +1,9 @@
 """
 This module contains the implementation of traversing accross list functionality.
 """
-
 nums=[1,2,4,5,6]
 for i in range(len(nums)):
-  print(nums[i])
-  print(i)
+    print(nums[i])
+    print(i)
   
   
